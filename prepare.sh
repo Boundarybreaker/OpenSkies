d880cdc64171aa417aca7f95565dd0baeea0357e
Meredith Espinosa <goclonefilms@gmail.com>
2017-12-31 16:43:43 -0800

    added prepare shell file necessary for skcraft

---
+++ prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+find ./src/ -name '*.jar' | while read file; do
+	if [ ! -e "${file}.url.txt" ]; then
+		echo Deleting dangling file `basename $file`...
+		rm "$file"
+	fi
+done
+find ./ -name '*.url.txt' -print0 | while read -d $'\0' file; do
+	basename=${file%.url.txt}
+	if [ ! -e "$basename" ]; then
+		url=`cat "$file"`
+		echo Downloading $url...
+		curl -L -s "$url" -o "$basename"
+	else
+		echo Skipping download of existing file `basename "$basename"`
+	fi
+done
